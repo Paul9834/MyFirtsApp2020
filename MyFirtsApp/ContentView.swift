40792ee38a9e83299880a216ada9faef42d0b736
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-08-20 18:24:00 -0500

    Initial Commit

---
+++ MyFirtsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirtsApp
+//
+//  Created by general on 20/08/20.
+//  Copyright © 2020 general. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
